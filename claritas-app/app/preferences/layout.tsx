a01e1444c338d00161c8188337262c2db6fe3cc8
Maanasi Ganapathy <maanasiganapathy@gmail.com>
2026-01-20 18:20:48 -0600

    Deleted pages folder (legacy) and moved preferences into app router

---
+++ claritas-app/app/preferences/layout.tsx
@@ -0,0 +1,7 @@
+export default function PreferencesLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <section>{children}</section>
+}
--- claritas-app/app/preferences/page.tsx
+++ claritas-app/app/preferences/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 export default function PreferencesLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <h1>Preferences</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Preferences</h1>
+        </div>
+    );
 }
